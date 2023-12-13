ced86c6bb12994dbb97da9bb915f30a9c4d97bfe
Andy Garcia <amgarcia.dev@gmail.com>
2023-12-12 19:38:40 -0800

    scripts

---
+++ scripts/practice.js
@@ -0,0 +1,41 @@
+console.log("Practice");
+
+//my info//
+
+let myName = "Andy";
+let myGrade = 4.0;
+let isProfessor = false;
+
+//display values
+console.log(myName);
+console.log(myGrade);
+console.log(isProfessor);
+
+//display value 2
+console.log("My name is " + myName);
+console.log("My GPA is " + myGrade);
+console.log("Active: " + isProfessor);
+
+// numbers
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Mulitiplication: " + num1 * num2);
+
+// Assignment 1
+let assignmentName = "Class 1 - Assingment 1";
+let description =
+  "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+let pointsAs1 = 6.66;
+let pointsAs2 = 6.66;
+let pointsAs3 = 6.66;
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads = true;
+let studentAnnotation = false;
+
+let dueDate = "12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);
